9795c1d4700d34ba2df7c9d0c33698da849171e4
Rithvik-K-T <160377437+Rithvik-K-T@users.noreply.github.com>
2024-07-27 12:56:31 +0530

    Update home.py

--- home.py
+++ home.py
@@ -5,13 +5,13 @@ dir_path = r'G:\\sft\\Music-Library\\Music\\'
 
 
 
-# list to store files
+
 res = []
 cmd=input()
 if (cmd=='<program> --input <path to music folder> --list-files'):
-# Iterate directory
+
     for path in os.listdir(dir_path):
-        # check if current path is a file
+        
         if os.path.isfile(os.path.join(dir_path, path)):
             res.append(path)
     print(res)
